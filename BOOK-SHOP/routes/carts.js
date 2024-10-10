5da7deec21d7b2cd9318d0b8399558eecb0debb5
meteorqz6 <nys8765@naver.com>
2024-10-10 23:02:50 +0900

    장바구니 상품 목록 구현

--- BOOK-SHOP/routes/carts.js
+++ BOOK-SHOP/routes/carts.js
@@ -1,24 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const {addToCart, getCartItems, deleteCartItems} = require('../controller/CartController');
 router.use(express.json());
 
-// 장바구니 담기
-router.post('/', (req, res) => {
-    res.json('장바구니 담기');
-});
-
-// 장바구니 조회
-router.get('/', (req, res) => {
-    res.json('장바구니 조회');
-});
-
-// 장바구니 도서 삭제
-router.delete('/:id', (req, res) => {
-    res.json('장바구니 도서 삭제');
-});
-
-// 장바구니에서 선택한 주문 예상 상품 목록 조회
-// router.get();
+router.post('/', addToCart); // 장바구니 담기
+router.get('/', getCartItems); // 장바구니 아이템 목록 조회 / 선택한 장바구니 상품 목록 조회
+router.delete('/:id', deleteCartItems); // 장바구니 도서 삭제
 
 module.exports = router;
